3b30c4df3221c55dfaed1d92b8ecc11d18ce3630
Viktor Strelianyi <chmpain@gmail.com>
2020-03-09 17:54:31 +0200

    - update

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,41 +1,41 @@
 import React from 'react';
 import s from './Dialogs.module.css';
-import DialogItem from "./DialogItem/DialogItem";
-import Message from "./Message/Message";
+import DialogItem from './DialogItem/DialogItem';
+import Message from './Message/Message';
 
-const Dialogs = (props) => {
+const Dialogs = ( props ) => {
 
-    let state = props.dialogsPage;
+  const state = props.dialogsPage;
 
-    let dialogsElements = state.dialogs.map( d => <DialogItem name={d.name} key={d.id} id={d.id} />  );
-    let messagesElements = state.messages.map( m => <Message message={m.message} key={m.id} /> );
-    let newMessageBody = state.newMessageBody;
+  const dialogsElements = state.dialogs.map( d => <DialogItem name={d.name} key={d.id} id={d.id} />  );
+  const messagesElements = state.messages.map( m => <Message message={m.message} key={m.id} /> );
+  const newMessageBody = state.newMessageBody;
 
-    let onSendMessageClick = () => {
-        props.sendMessage();
-    }
+  const onSendMessageClick = () => {
+    props.sendMessage();
+  };
 
-    let onNewMessageChange = (e) => {
-        let body = e.target.value;
-        props.updateNewMessageBody(body);
-    }
+  const onNewMessageChange = ( e ) => {
+    const body = e.target.value;
+    props.updateNewMessageBody( body );
+  };
 
-    return (
-        <div className={s.dialogs}>
-            <div className={s.dialogsItems}>
-                { dialogsElements }
-            </div>
-            <div className={s.messages}>
-                <div>{ messagesElements }</div>
-                <div>
-                    <div><textarea value={newMessageBody}
-                                   onChange={onNewMessageChange}
-                                   placeholder='Enter your message'></textarea></div>
-                    <div><button onClick={onSendMessageClick}>Send</button></div>
-                </div>
-            </div>
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        { dialogsElements }
+      </div>
+      <div className={s.messages}>
+        <div>{ messagesElements }</div>
+        <div>
+          <div><textarea value={newMessageBody}
+            onChange={onNewMessageChange}
+            placeholder='Enter your message'></textarea></div>
+          <div><button onClick={onSendMessageClick}>Send</button></div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Dialogs;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import ProfileInfo from "./ProfileInfo/ProfileInfo";
-import MyPostsContainer from "./MyPosts/MyPostsContainer";
+import ProfileInfo from './ProfileInfo/ProfileInfo';
+import MyPostsContainer from './MyPosts/MyPostsContainer';
 
 const Profile = () => {
-    return (
-        <div>
-            <ProfileInfo />
-            <MyPostsContainer />
-        </div>
-    )
-}
+  return (
+    <div>
+      <ProfileInfo />
+      <MyPostsContainer />
+    </div>
+  );
+};
 
 export default Profile;
