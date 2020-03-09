0abf8397d45dc64c233d59a1c4c907c6a448f260
Viktor Strelianyi <chmpain@gmail.com>
2020-03-09 17:30:47 +0200

    - update

--- src/App.js
+++ src/App.js
@@ -3,29 +3,29 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import {Route} from "react-router-dom";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import UsersContainer from "./components/Users/UsersContainer";
+import { Route } from 'react-router-dom';
+import DialogsContainer from './components/Dialogs/DialogsContainer';
+import UsersContainer from './components/Users/UsersContainer';
 
-const App = () => {
-    return (
-            <div className='app-wrapper'>
-                <Header />
-                <Navbar />
-                <div className='app-wrapper-content'>
-                    <Route path='/dialogs'
-                           render={ () => <DialogsContainer /> }/>
+const App = () =>  {
+  return (
+    <div className='app-wrapper'>
+      <Header />
+      <Navbar />
+      <div className='app-wrapper-content'>
+        <Route path='/dialogs'
+          render={ () => <DialogsContainer /> }/>
 
-                    <Route path='/profile'
-                           render={ () => <Profile /> }/>
+        <Route path='/profile'
+          render={ () => <Profile /> }/>
 
-                    <Route path='/users'
-                           render={ () => <UsersContainer /> }/>
+        <Route path='/users'
+          render={ () => <UsersContainer /> }/>
 
 
-                </div>
-            </div>
-        )
-}
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,27 +1,26 @@
-import React from 'react';
-import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
-import MyPosts from "./MyPosts";
-import {connect} from "react-redux";
+import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/profile-reducer';
+import MyPosts from './MyPosts';
+import { connect } from 'react-redux';
 
-const mapStateToProps = (state) => {
-    return {
-        posts: state.profilePage.posts,
-        newPostText: state.profilePage.newPostText
-    }
-}
+const mapStateToProps = ( state ) => {
+  return {
+    posts: state.profilePage.posts,
+    newPostText: state.profilePage.newPostText,
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        updateNewPostText: (text) => {
-            let action = updateNewPostTextActionCreator(text);
-            dispatch(action);
-        },
-        addPost: () => {
-            dispatch(addPostActionCreator());
-        }
-    }
-}
+const mapDispatchToProps = ( dispatch ) => {
+  return {
+    updateNewPostText: ( text ) => {
+      const action = updateNewPostTextActionCreator( text );
+      dispatch( action );
+    },
+    addPost: () => {
+      dispatch( addPostActionCreator() );
+    },
+  };
+};
 
-const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
+const MyPostsContainer = connect( mapStateToProps, mapDispatchToProps )( MyPosts );
 
 export default MyPostsContainer;
